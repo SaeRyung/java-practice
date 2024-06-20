131ff28d0bd767e5d4967865d32b00522131d20e
SaeRyung <sr1094@naver.com>
2024-06-20 14:43:24 +0900

    주석

---
+++ src/sec02/chap02/Main.java
@@ -0,0 +1,17 @@
+package sec02.chap02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+//          사뿐히 즈려밟고 가시옵소서
+//          System.out.println("먼저 가, 난 틀렸어.");
+        //  보기 편하게 두번 스페이스
+        System.out.println("헬로 월드"); //  주석이 요기잉네
+
+        /*
+        주석 여러줄
+        코드 비활성화 등
+        */
+    }
+}
