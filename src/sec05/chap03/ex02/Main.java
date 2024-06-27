f5427641608aac15c57a896c5ca496ac442b317b
SaeRyung <sr1094@naver.com>
2024-06-27 15:14:25 +0900

    sec05_chap03 클래스(정적)필드와 메소드

---
+++ src/sec05/chap03/ex02/Main.java
@@ -0,0 +1,17 @@
+package sec05.chap03.ex02;
+
+import sec05.chap03.ex03.Button;
+
+public class Main {
+    public static void main(String[] args) {
+        YalcoChicken store1 = new YalcoChicken("판교");
+        YalcoChicken store2 = new YalcoChicken("강남");
+        YalcoChicken store3 = new YalcoChicken("제주");
+
+        //  연속으로 붙여넣어 실행해볼것
+//        Button.switchMode();
+//
+//        System.out.println(Button.mode);
+//        for(Button button : buttons){button.place();}
+    }
+}
